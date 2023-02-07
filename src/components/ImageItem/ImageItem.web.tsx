c9fab385bd8e83dfac03cec6f3a5565ac2f4b39d
Andre Staltz <andre@staltz.com>
2023-02-07 12:09:52 +0200

    remove zooming from ImageItem on react-native-web

--- src/components/ImageItem/ImageItem.web.tsx
+++ src/components/ImageItem/ImageItem.web.tsx
@@ -1,10 +1,28 @@
-/**
- * Copyright (c) JOB TODAY S.A. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
+import React, { useState, useCallback } from "react";
+import { View, Image, Dimensions } from "react-native";
+import useImageDimensions from "../../hooks/useImageDimensions";
+import { ImageSource } from "../../@types";
+import { ImageLoading } from "./ImageLoading";
 
-import ImageItem from "./ImageItem.android";
-export default ImageItem;
+type Props = {
+  imageSrc: ImageSource;
+};
+
+const ImageItem = ({ imageSrc }: Props) => {
+    const SCREEN = Dimensions.get('window');
+    const SCREEN_WIDTH = SCREEN.width;
+    const SCREEN_HEIGHT = SCREEN.height;
+    const imageDimensions = useImageDimensions(imageSrc);
+    const [isLoaded, setLoadEnd] = useState(false);
+    const onLoaded = useCallback(() => setLoadEnd(true), []);
+    const imagesStyles = imageDimensions ? {
+        width: '100%',
+        height: '100%',
+    } : null;
+    return (<View style={{ width: SCREEN_WIDTH, height: SCREEN_HEIGHT, flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      <Image source={imageSrc} style={imagesStyles} resizeMode={'contain'} onLoad={onLoaded}/>
+      {(!isLoaded || !imageDimensions) && <ImageLoading />}
+    </View>);
+};
+export default React.memo(ImageItem);
+
